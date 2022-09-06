fabc974a6ba5e081f1a00f10013cb604db45ecbd
Theo <theo.le.wiik@gmail.com>
2022-09-06 20:23:50 +0200

    Basic structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,48 @@
 import type { NextPage } from 'next';
 
+type Row = {
+  content: string;
+  children: Row[];
+};
+
 const Home: NextPage = () => {
+  const wasd: string = 'wasd';
+
+  const roots: Row[] = [
+    {
+      content: 'root',
+      children: [
+        {
+          content: 'child1',
+          children: [{ content: 'grandchild1', children: [] }],
+        },
+      ],
+    },
+  ];
+
   return (
-    <div>
-      <h1>carabiner</h1>
-      <p>Some text</p>
+    <div className="container mt-5">
+      <h1 className="is-size-1 has-text-weight-bold">carabiner</h1>
+
+      <p className="mt-5 mb-5">sample</p>
+
+      {roots.map((row, index) => {
+        const x = 'test';
+
+        function subtree(tree: Row): any {
+          return (
+            <div>
+              <p>{tree?.content}</p>
+
+              {tree.children.map((child) => {
+                return subtree(child);
+              })}
+            </div>
+          );
+        }
+
+        return subtree(row);
+      })}
     </div>
   );
 };
